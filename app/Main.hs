fc5ac86b9c2f3118dcc5a36cb581cdc24ad408de
yab <tajemniczychlopiec@gmail.com>
2016-05-13 10:07:21 +0200

    almsot

--- app/Main.hs
+++ app/Main.hs
@@ -1,80 +1,79 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Main where
-import BasePrelude
-import System.Directory
-import Data.Witherable (hashNub)
-import Data.Array
+import           BasePrelude
+import           Data.Witherable  (hashNub)
+import           System.Directory
+import           System.FilePath
+import           System.IO
 
 main :: IO ()
 main = do
-  files <- mapM readFile =<< books
-  let merged = hashNub $ words $ concatMap (map cleanChar) files
---["word", "word", "word"]
+  hSetBuffering stdin LineBuffering
+  allWords <- getAllWords
+  let matchAllWords = matchWords allWords
   putStrLn "How many letters are there?\n eg. \"4 6\""
-  lettNum <- words <$> getLine
---["3", "4", "7"]
+  wordLengths <- map read . words <$> getLine
   putStrLn "What letters do you have? Type them one by one, lowercase."
-  letters <- getLine
---["a", "s", "d"]
-  let matched = catMaybes $ matchWords merged letters []
---[("word", "r e m"),("word", "r e m")]
+  letters <- filter isLetter <$> getLine
+  let matched = matchAllWords letters
   print matched
-  print $ filterMatches matched lettNum
+  print "-----------------------------------------"
+  let filtered = filterMatches matched wordLengths
+  print filtered
+  print "-----------------------------------------"
+  let matched2 = map matchAllWords (map snd filtered)
+  mapM_ print matched2
+  print "-----------------------------------------"
+  let matched3 = zipWith extendTuple (concat matched2) (map fst filtered)
+  print matched3
+  print "-----------------------------------------"
+  --let filtered2 = filterMatches matched3
+  --print filtered2
+--[("word", "r e m"),("word", "r e m")]
   --print $ map reverse $ map head $ catMaybes matched
 
-type WordNRem = Maybe ([Char], [Char])
---("word", "s a e w")
+getAllWords :: IO [String]
+getAllWords = do
+  bookFiles <- map ("data" </>) <$> listDirectory "data"
+  books <- mapM readFile bookFiles
+  --writeFile "data/merged" books
+  let sanitize c | isLetter c = c
+                 | otherwise = ' '
+  let getWords = words . map sanitize
+  return $ hashNub $ concatMap getWords books
 
 bookNames :: IO [FilePath]
 bookNames = listDirectory "data"
 
 books :: IO [FilePath]
 books = map ("data/" ++) <$> bookNames
---["data/book", "data/book"]
 
-cleanChar :: Char -> Char
-cleanChar ch
-  | isLetter ch = ch
-  | otherwise = ' '
+matchWord :: String -> [Char] -> Maybe (String, [Char])
+matchWord word letters = go word letters
+  where
+    go [] ls        = Just (word, ls)
+    go (c:cs) ls
+      | c `elem` ls = go cs (delete c ls)
+      | otherwise   = Nothing
 
-matchWord :: String -> String -> String -> WordNRem
--- ch is char in a word, lett is letter list
-matchWord (ch:chs) lett accum
-  | null chs && ch `elem` lett = Just (ch:accum, (delete ch lett))
-  | ch `elem` lett  = matchWord chs (delete ch lett) (ch:accum)
-  | ch `notElem` lett = Nothing
+matchWords :: [String] -> [Char] -> [(String, [Char])]
+matchWords ws letters = mapMaybe (`matchWord` letters) ws
 
-matchWords :: [String] -> String -> String -> [WordNRem]
-matchWords lst lett accum = map (\w -> matchWord w lett accum) lst
+filterMatches :: [(String, String)] -> [Int] -> [(String, String)]
+filterMatches lst lett = filter (p . reverse . fst) lst
+  where p x = length x == head lett
 
-filterMatches :: [([a], b)] -> [String] -> [[a]]
-filterMatches lst lett = filter f (revHeads lst)
-  where f = (\x -> length x == (read (head lett) :: Int))
-        revHeads = fmap (reverse . fst)
+findMatches :: [String] -> [Char] -> [([String], String)]
+findMatches allWords allLetters =
+  case matchWords allWords allLetters of
+  -- no way to match even a single word
+    [] -> [([], allLetters)]
+    matches -> do
+      (match, leftovers) <- matches
+      -- find all ways to match all other words with leftover letters
+      (wordSet, finalLeftOvers) <- findMatches allWords leftovers
+      return (match:wordSet, finalLeftOvers)
 
-mainLoop :: [WordNRem] -> [String] -> [String] -> [String] -> Maybe String
-mainLoop matched lst lett accum
-  | null matched = Nothing
---let matched = catMaybes $ matchWords merged letters []
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---mainLoop lettNum matched l merged
---  | null lettNum = revHeads matched
---  | otherwise = map (matchWords merged)
+extendTuple :: (a, b) -> c -> (c, a, b)
+extendTuple tuple e = (e, (fst tuple), (snd tuple))
